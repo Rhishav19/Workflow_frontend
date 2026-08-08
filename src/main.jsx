a43dcea8580d459a9e185f5a90857fa1656a760a
Aashirbad Khanal <aashirbad.231602@ncit.edu.np>
2026-08-08 19:44:28 +0545

    Changes to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { WorkspaceProvider } from "./context/WorkspaceContext.jsx";
@@ -8,15 +9,13 @@ import { ProjectsProvider } from "./context/ProjectsContext.jsx";
 import { TasksProvider } from "./context/TasksContext.jsx";
 import { TimeTrackingProvider } from "./context/TimeTrackingContext.jsx";
 import { MembersProvider } from "./context/MembersContext.jsx";
-import "./index.css";
-<<<<<<< Updated upstream
-
-=======
 import { AnnouncementsProvider } from "./context/AnnouncementContext.jsx";
 import { DocsProvider } from "./context/DocsContext.jsx";
+import { ActivityProvider } from "./context/ActivityContext.jsx";
+import { NotificationsProvider } from "./context/NotificationsContext.jsx";
 
-import { ActivityProvider } from "./context/ActivityContext.jsx";import { NotificationsProvider } from "./context/NotificationsContext.jsx";
->>>>>>> Stashed changes
+import "./index.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
@@ -26,9 +25,6 @@ createRoot(document.getElementById("root")).render(
             <TasksProvider>
               <TimeTrackingProvider>
                 <MembersProvider>
-<<<<<<< Updated upstream
-                  <App />
-=======
                   <AnnouncementsProvider>
                     <ActivityProvider>
                       <DocsProvider>
@@ -38,7 +34,6 @@ createRoot(document.getElementById("root")).render(
                       </DocsProvider>
                     </ActivityProvider>
                   </AnnouncementsProvider>
->>>>>>> Stashed changes
                 </MembersProvider>
               </TimeTrackingProvider>
             </TasksProvider>
